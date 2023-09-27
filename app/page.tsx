2c50bfe115fbca79d7161da975897fab33b956f5
peterdresslar <peterdresslar@gmail.com>
2023-09-26 21:43:40 -1000

    exponential decay

--- app/page.tsx
+++ app/page.tsx
@@ -4,54 +4,93 @@ import { useEffect, useState } from 'react';
 import * as d3 from 'd3';
 
 export default function Home() {
+  const maxRadius = 25;
   const [radius, setRadius] = useState(1);
   const diameter = radius * 2;
   const circumference = Math.round(((diameter * Math.PI) + Number.EPSILON) * 1000) / 1000
+
+  // referenceAngle is the reference angle of the overlays (radius, diameter, etc.) for the circle
+  // To calculate motion, we can simply move our radius and our referenceAngle.
+  // We could possibly use a vertex, but no plans for that yet
+  const startingReferenceAngle = 10;
+  const [referenceAngle, setReferenceAngle] = useState(startingReferenceAngle);
+  // rotationVelocity is velocity of the circle in degrees per second. can be positive or negative.
+  const startingRotationVelocity = .001;
+  const [rotationVelocity, setRotationVelocity] = useState(startingRotationVelocity);
+  // radiusVelocity is velocity of the radius expansion or contraction in px per second. can be positive or negative.
+  // note maybe should be implemented as percentage of min/max radius
+  const startingRadiusVelocity = 1;
+  const [radiusVelocity, setRadiusVelocity] = useState(startingRadiusVelocity);
+  // friction is a constant that gives motion a sticky feeling. 0 is no friction, 1 is max friction
+  const radiusFriction = .85
+  const [radiusDelta, setRadiusDelta] = useState(1.5);
+  const rotationFriction = 0.9;
+  const [rotationDelta, setRotationDelta] = useState(10);
 
   const area = Math.round((Math.PI * Math.pow(radius, 2) + Number.EPSILON) * 1000) / 1000;
   const svgWidth = 600;
   const svgHeight = 600;
 
   useEffect(() => {
-    // Select the SVG element
+    let animationFrameId: number | undefined
+
+    const animate = () => {
+      // Update the state variables
+      setRotationDelta(prevDelta => prevDelta * rotationFriction);
+      setRotationVelocity(prevVelocity => prevVelocity * rotationDelta);
+      setRadiusDelta(prevDelta => prevDelta * radiusFriction);
+      setRadiusVelocity(prevVelocity => prevVelocity * radiusDelta);
+      setRadius(prevRadius => Math.min(maxRadius, Math.max(1, prevRadius + radiusVelocity)));
+      setReferenceAngle(prevAngle => prevAngle + rotationVelocity);
+
+      animationFrameId = requestAnimationFrame(animate);
+    };
+
+    // Start the animation
+    animationFrameId = requestAnimationFrame(animate);
+
+    return () => {
+      // Cleanup: cancel the animation frame
+      if (animationFrameId) {
+      cancelAnimationFrame(animationFrameId);
+      }
+    };
+  }, [radius, referenceAngle, radiusVelocity, rotationVelocity, radiusDelta, rotationDelta, radiusFriction, rotationFriction]);
+
+  useEffect(() => {
     const svg = d3.select('#circles-svg');
-    
-    // Clear previous elements
-    svg.selectAll("*").remove();
-    
-    // Calculate scaled radius
+    svg.selectAll('*').remove();
+
     const scaledRadius = Math.min(svgWidth, svgHeight) / 2 * (radius / 25);
-    
-    // Compute the color of the circle
-    // We want to go around the color scale twice with a small offset for flavor!
-    const fillValue = Math.round((radius / 25) * 720) + 5;
-    const fill = `hsl(${fillValue}, 100%, 50%)`
-    console.log(fillValue);
-    
-    // Add a circle
+    const fill = `hsl(${Math.round((radius / 25) * 360)}, 100%, 50%)`;
+
+    // Draw the circle
     svg.append('circle')
       .attr('cx', svgWidth / 2)
       .attr('cy', svgHeight / 2)
       .attr('r', scaledRadius)
       .attr('fill', fill);
-    
-    // Add radius line
+
+    // Convert reference angle to radians
+    const angleInRadians = (referenceAngle * Math.PI) / 180;
+
+    // Draw the radius line
     svg.append('line')
       .attr('x1', svgWidth / 2)
       .attr('y1', svgHeight / 2)
-      .attr('x2', svgWidth / 2)
-      .attr('y2', (svgHeight / 2) - scaledRadius)
-      .attr('stroke', (fillValue % 360 > 210)  ? 'white' : 'black') // change this based on your actual fill logic
+      .attr('x2', svgWidth / 2 + Math.cos(angleInRadians) * scaledRadius)
+      .attr('y2', svgHeight / 2 - Math.sin(angleInRadians) * scaledRadius)
+      .attr('stroke', 'black')
       .attr('stroke-width', 3);
-    
-    // Add radius label
+
+    // Draw the label
     svg.append('text')
-      .attr('x', svgWidth / 2 + 10)  // 10 pixels right of the line end
-      .attr('y', (svgHeight / 2) - scaledRadius - 4) // align with the line end and offset
+      .attr('x', svgWidth / 2 + Math.cos(angleInRadians) * scaledRadius + 10)
+      .attr('y', svgHeight / 2 - Math.sin(angleInRadians) * scaledRadius - 4)
       .attr('font-size', '16px')
       .attr('text-anchor', 'middle')
-      .text(`${radius}`);
-  }, [radius]);
+      .text(`${radius.toFixed(2)}`);
+  }, [radius, referenceAngle]);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -72,6 +111,9 @@ export default function Home() {
     <div className="w-36 items-center gap-2 justify-between font-mono text-sm lg:flex">
       <label htmlFor="radius">Set The Radius!</label>
       <input type="range" min="1" max="25" value={radius} onChange={(e) => setRadius(Number(e.target.value))} />
+    </div>
+    <div>
+      {radiusVelocity.toFixed(2)} {radiusDelta.toFixed(2)} {rotationVelocity.toFixed(2)} { rotationDelta.toFixed(2)}
     </div>
   </main>
   )
